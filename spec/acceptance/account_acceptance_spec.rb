0c41d2ff64d56c0275e45b80ca09131a15c5f493
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-01-10 23:24:19 +0100

    Move parse_json to spec_helper.rb

--- spec/acceptance/account_acceptance_spec.rb
+++ spec/acceptance/account_acceptance_spec.rb
@@ -158,8 +158,4 @@ describe Scrooge do
       end
     end
   end
-
-  def parse_json(json_response)
-    JSON.parse(json_response.body, symbolize_names: true)
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
 require 'simplecov'
 SimpleCov.start
+
+def parse_json(json_response)
+  JSON.parse(json_response.body, symbolize_names: true)
+end
