00b1d57f0f5ab1a32ffe19836c82d40ee64ae7bd
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-01-12 01:58:26 +0100

    Add db setup to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
+# Setup SimpleCov
+
 require 'simplecov'
 SimpleCov.start
+
+# Setup DataMapper
+
+require 'data_mapper'
+DataMapper.setup(:default, 'sqlite3::memory:')
+DataMapper.auto_migrate!
+
+# Helper functions
 
 def parse_json(json_response)
   JSON.parse(json_response.body, symbolize_names: true)
