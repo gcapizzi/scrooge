63383b7bd3a412faaddafd2838f911854e6cf52d
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-04-03 09:36:45 +0200

    First Ember experiment

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,21 @@
 //= require foundation
+
+window.App = Ember.Application.create();
+
+App.Account = Ember.Object.extend({});
+
+App.Account.reopenClass({
+  findAll: function() {
+    return $.getJSON('/accounts').then(function(response) {
+      return response.accounts.map(function(account) {
+        return App.Account.create(account.account);
+      });
+    });
+  }
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return App.Account.findAll();
+  }
+});
