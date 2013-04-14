5cc6a8e9bd2225994bd27fd722f7d0c694e4e402
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-04-14 16:24:10 +0200

    DRY up renderers

--- app/renderers.rb
+++ app/renderers.rb
@@ -2,17 +2,9 @@ require 'rabl'
 
 module Scrooge
 
-  class AccountJsonRenderer
+  class JsonRenderer
     def initialize(view_path)
       @view_path = view_path
-    end
-
-    def render_object(account)
-      render(account, 'account')
-    end
-
-    def render_collection(accounts)
-      render(accounts, 'accounts')
     end
 
     private
@@ -22,23 +14,23 @@ module Scrooge
     end
   end
 
-  class TransactionJsonRenderer
-    def initialize(view_path)
-      @view_path = view_path
+  class AccountJsonRenderer < JsonRenderer
+    def render_object(account)
+      render(account, 'account')
     end
 
+    def render_collection(accounts)
+      render(accounts, 'accounts')
+    end
+  end
+
+  class TransactionJsonRenderer < JsonRenderer
     def render_object(transaction)
       render(transaction, 'transaction')
     end
 
     def render_collection(transactions)
       render(transactions, 'transactions')
-    end
-
-    private
-
-    def render(object, template)
-      Rabl.render(object, template, view_path: @view_path)
     end
   end
 end
