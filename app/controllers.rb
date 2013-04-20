5fb3d944690222a2046ed3ad1acf0b5d06045656
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-04-20 16:13:31 +0200

    model_class -> model_collection

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,19 +1,19 @@
 module Scrooge
 
   class Controller
-    def initialize(model_class, renderer)
-      @model_class = model_class
+    def initialize(model_collection, renderer)
+      @model_collection = model_collection
       @renderer = renderer
     end
 
     def index
-      collection = @model_class.all
+      collection = @model_collection.all
       body = @renderer.render_collection(collection)
       [200, [body]]
     end
 
     def show(id)
-      object = @model_class.get(id.to_i)
+      object = @model_collection.get(id.to_i)
       return [404, []] if object.nil?
       body = @renderer.render_object(object)
       [200, [body]]
--- spec/unit/controllers_unit_spec.rb
+++ spec/unit/controllers_unit_spec.rb
@@ -6,15 +6,14 @@ module Scrooge
 
   describe Controller do
     let(:renderer) { double('renderer') }
-    let(:model_class) { double('model_class') }
-    let(:controller) { Controller.new(model_class, renderer) }
+    let(:model_collection) { double('model_collection') }
+    let(:controller) { Controller.new(model_collection, renderer) }
 
     describe '#index' do
       it 'returns all objects' do
-        model_collection = double('model collection')
         json_response = 'Some JSON'
 
-        model_class.should_receive(:all).and_return(model_collection)
+        model_collection.should_receive(:all).and_return(model_collection)
         renderer.should_receive(:render_collection).with(model_collection).and_return(json_response)
 
         response = controller.index
@@ -29,7 +28,7 @@ module Scrooge
           json_response = 'Some JSON'
           model_object = double('model object')
 
-          model_class.should_receive(:get).with(id).and_return(model_object)
+          model_collection.should_receive(:get).with(id).and_return(model_object)
           renderer.should_receive(:render_object).with(model_object).and_return(json_response)
 
           response = controller.show(id)
@@ -40,7 +39,7 @@ module Scrooge
       context 'when the object doesn\'t exist' do
         it 'returns 404' do
           id = 123
-          model_class.should_receive(:get).with(id).and_return(nil)
+          model_collection.should_receive(:get).with(id).and_return(nil)
           renderer.should_not_receive(:render_object)
 
           response = controller.show(id)
