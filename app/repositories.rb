7e8e7c5c95ed6c5713165960fac23f49005a9525
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-06-11 12:19:12 +0200

    Add Repositories module

--- app/repositories.rb
+++ app/repositories.rb
@@ -1,33 +1,35 @@
 module Scrooge
+  module Repositories
 
-  class SequelRepository
-    def initialize(model_collection)
-      @model_collection = model_collection
+    class SequelRepository
+      def initialize(model_collection)
+        @model_collection = model_collection
+      end
+
+      def get(id)
+        @model_collection[id]
+      end
+
+      def all
+        @model_collection.all
+      end
+
+      def create(params)
+        @model_collection.create(params)
+      end
+
+      def update(object)
+        object.save
+      end
+
+      def destroy(object)
+        object.destroy
+      end
+
+      def attributes
+        @model_collection.columns - [:id]
+      end
     end
 
-    def get(id)
-      @model_collection[id]
-    end
-
-    def all
-      @model_collection.all
-    end
-
-    def create(params)
-      @model_collection.create(params)
-    end
-
-    def update(object)
-      object.save
-    end
-
-    def destroy(object)
-      object.destroy
-    end
-
-    def attributes
-      @model_collection.columns - [:id]
-    end
   end
-
 end
--- spec/integration/repositories_integration_spec.rb
+++ spec/integration/repositories_integration_spec.rb
@@ -4,94 +4,96 @@ require './app/models'
 require './app/repositories'
 
 module Scrooge
+  module Repositories
 
-  shared_examples 'a repository' do
-    let(:account) { @accounts.first }
+    shared_examples 'a repository' do
+      let(:account) { @accounts.first }
 
-    describe '#get' do
-      context 'when the object exists' do
-        it 'gets an object from the database' do
-          expect(subject.get(account.id)).to eq(account)
+      describe '#get' do
+        context 'when the object exists' do
+          it 'gets an object from the database' do
+            expect(subject.get(account.id)).to eq(account)
+          end
+        end
+
+        context 'when the object doesn\'t exist' do
+          it 'returns nil' do
+            expect(subject.get(123)).to be_nil
+          end
         end
       end
 
-      context 'when the object doesn\'t exist' do
-        it 'returns nil' do
-          expect(subject.get(123)).to be_nil
+      describe '#all' do
+        it 'returns all objects in the collection' do
+          expect(subject.all).to eq(@accounts)
+        end
+      end
+
+      describe '#create' do
+        context 'when params are valid' do
+          it 'creates a new object and returns it' do
+            name = 'an account'
+            object = subject.create(name: name)
+            expect(object.name).to eq(name)
+            expect(subject.get(object.id)).to eq(object)
+          end
+        end
+
+        context 'when params are invalid' do
+          it 'returns nil and doesn\'t create a record' do
+            object = subject.create(name: '')
+            expect(object).to be_nil
+            expect(subject.all.count).to eq(3)
+          end
+        end
+      end
+
+      describe '#update' do
+        context 'when params are valid' do
+          it 'returns true and updates the object' do
+            new_name = 'a new name'
+            account.name = new_name
+            updated = subject.update(account)
+            expect(updated).to be_true
+            expect(subject.get(account.id)).to eq(account)
+          end
+        end
+
+        context 'when params are invalid' do
+          it 'returns false and doesn\'t update the object' do
+            account.name = ''
+            updated = subject.update(account)
+            expect(updated).to be_false
+            expect(subject.get(account.id).name).not_to be_empty
+          end
+        end
+      end
+
+      describe '#destroy' do
+        it 'destroys the object' do
+          destroyed = subject.destroy(account)
+          expect(destroyed).to be_true
+          expect(subject.get(account.id)).to be_nil
+          expect(subject.all.count).to eq(2)
+        end
+      end
+
+      describe '#attributes' do
+        it 'returns the list of the model attributes, except the id' do
+          expect(subject.attributes).to eq([:name])
         end
       end
     end
 
-    describe '#all' do
-      it 'returns all objects in the collection' do
-        expect(subject.all).to eq(@accounts)
+    describe SequelRepository do
+      before(:each) do
+        Models::Account.destroy
+        @accounts = (1..3).map { |i| Models::Account.create(name: "account #{i}") }
       end
+
+      subject { SequelRepository.new(Models::Account) }
+      it_behaves_like 'a repository'
     end
 
-    describe '#create' do
-      context 'when params are valid' do
-        it 'creates a new object and returns it' do
-          name = 'an account'
-          object = subject.create(name: name)
-          expect(object.name).to eq(name)
-          expect(subject.get(object.id)).to eq(object)
-        end
-      end
-
-      context 'when params are invalid' do
-        it 'returns nil and doesn\'t create a record' do
-          object = subject.create(name: '')
-          expect(object).to be_nil
-          expect(subject.all.count).to eq(3)
-        end
-      end
-    end
-
-    describe '#update' do
-      context 'when params are valid' do
-        it 'returns true and updates the object' do
-          new_name = 'a new name'
-          account.name = new_name
-          updated = subject.update(account)
-          expect(updated).to be_true
-          expect(subject.get(account.id)).to eq(account)
-        end
-      end
-
-      context 'when params are invalid' do
-        it 'returns false and doesn\'t update the object' do
-          account.name = ''
-          updated = subject.update(account)
-          expect(updated).to be_false
-          expect(subject.get(account.id).name).not_to be_empty
-        end
-      end
-    end
-
-    describe '#destroy' do
-      it 'destroys the object' do
-        destroyed = subject.destroy(account)
-        expect(destroyed).to be_true
-        expect(subject.get(account.id)).to be_nil
-        expect(subject.all.count).to eq(2)
-      end
-    end
-
-    describe '#attributes' do
-      it 'returns the list of the model attributes, except the id' do
-        expect(subject.attributes).to eq([:name])
-      end
-    end
   end
-
-  describe SequelRepository do
-    before(:each) do
-      Models::Account.destroy
-      @accounts = (1..3).map { |i| Models::Account.create(name: "account #{i}") }
-    end
-
-    subject { SequelRepository.new(Models::Account) }
-    it_behaves_like 'a repository'
-  end
-
 end
