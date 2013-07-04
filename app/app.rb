2212cfce3e49f77dd2feba394b453e39329fdbaa
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-07-04 14:33:11 +0200

    Fix regexes for some routes

--- app/app.rb
+++ app/app.rb
@@ -20,9 +20,9 @@ module Scrooge
 
   App = Rack::Mount::RouteSet.new do |app|
     app.add_route list_accounts,  { request_method: 'GET',    path_info: %r{^/accounts$}                    }, {}, :list_accounts
-    app.add_route show_account,   { request_method: 'GET',    path_info: %r{^/accounts/(?<account_id>\d+)} }, {}, :show_account
-    app.add_route update_account, { request_method: 'PATCH',  path_info: %r{^/accounts/(?<account_id>\d+)} }, {}, :update_account
+    app.add_route show_account,   { request_method: 'GET',    path_info: %r{^/accounts/(?<account_id>\d+)$} }, {}, :show_account
+    app.add_route update_account, { request_method: 'PATCH',  path_info: %r{^/accounts/(?<account_id>\d+)$} }, {}, :update_account
     app.add_route create_account, { request_method: 'POST',   path_info: %r{^/accounts$}                    }, {}, :create_account
-    app.add_route delete_account, { request_method: 'DELETE', path_info: %r{^/accounts/(?<account_id>\d+)} }, {}, :delete_account
+    app.add_route delete_account, { request_method: 'DELETE', path_info: %r{^/accounts/(?<account_id>\d+)$} }, {}, :delete_account
   end
 end
